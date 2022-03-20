18bce0a87a2e975790ef4aa01726cb21d2631e0e
Olesyaiam <olesyaiam13@mail.ru>
2022-03-20 15:28:01 +0200

    Head moves and measures distance

--- ArduinoCarConstrol/TestHead/TestHead.ino
+++ ArduinoCarConstrol/TestHead/TestHead.ino
@@ -1,17 +1,28 @@
 
 #include <Servo.h>
+#include "SR04.h"
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 90;    // variable to store the servo position
+// ultrasonic block
+const int TRIG_PIN = 8;
+const int ECHO_PIN = 7;
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 
+
+// servo block
+const int SERVO_PIN = 9;
 const int RIGHT_POS = 0;
 const int CENTER_POS = 90;
 const int LEFT_POS = 180;
+int pos = 90;    // variable to store the servo position
+
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600); // speed for console
+  
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin 9 to the servo object
   myservo.write(pos);
   delay(500); 
 }
@@ -19,34 +30,65 @@ void setup() {
 void goFromRigthToCenter() {
   for (pos = RIGHT_POS; pos <= CENTER_POS; pos += 1) { 
     // in steps of 1 degree
-    myservo.write(pos);             
-    delay(15);              
+    myservo.write(pos);
+    int ostatDelen = pos % 10;
+
+    if(ostatDelen == 0){
+      checkDistance(); 
+    } else{              
+      delay(15);
+    }                
   }
 }
 
 void goFromCenterToLeft() {
   for (pos = CENTER_POS; pos <= LEFT_POS; pos += 1) { 
     // in steps of 1 degree
-    myservo.write(pos);             
-    delay(15);                       
+    myservo.write(pos);
+    int ostatDelen = pos % 10;
+
+    if(ostatDelen == 0){
+      checkDistance(); 
+    } else{              
+      delay(15);
+    }                         
   }
 }
 
 void goFromLeftToCenter() {
   for (pos = LEFT_POS; pos >= CENTER_POS; pos -= 1) { 
-    myservo.write(pos);             
-    delay(15);                      
+    myservo.write(pos);
+    int ostatDelen = pos % 10;
+
+    if(ostatDelen == 0){
+      checkDistance(); 
+    }else{              
+      delay(15);
+    }                        
   }
 }
 
 void goFromCenterToRight() {
   for (pos = CENTER_POS; pos >= RIGHT_POS; pos -= 1) {
-    myservo.write(pos);              
-    delay(15);                       
+    myservo.write(pos);
+
+    int ostatDelen = pos % 10;
+
+    if(ostatDelen == 0){
+      checkDistance(); 
+    }else{              
+      delay(15);
+    }                       
   }
 }
 
-void loop() {
+void checkDistance(){
+  long distance = sr04.Distance();
+  Serial.print(distance);
+  Serial.println("cm");
+}
+
+void loop() {  
   goFromCenterToLeft();
   goFromLeftToCenter();
   delay(100);
